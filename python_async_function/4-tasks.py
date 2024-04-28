65488c495a591e15650d30e0d3c14a7a29df83c6
amenallahg122 <bigpapiamen@gmail.com>
2024-04-29 00:00:35 +0200

    python async function

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""ma tayassara mtasks"""
+from typing import List
+import asyncio
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Wait for n seconds and return a random number between 1 to max_delay.
+    """
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delay_list = await asyncio.gather(*coroutines)
+    return sorted(delay_list)
